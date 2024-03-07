035e518221f5cfa8fed2177aedf5099bc721caf1
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-06 21:41:52 -0300

    Feat: Adding Initial migrations and MigrationHandler.

---
+++ MonolithicArchitecture.API/Migrations/20240307003925_Initial.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MonolithicArchitecture.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Certifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    license_number = table.Column<string>(type: "varchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Certifications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    address = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specialities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    experience_years = table.Column<int>(type: "integer", nullable: false),
+                    birth_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    CertificationId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Doctors_Certifications_CertificationId",
+                        column: x => x.CertificationId,
+                        principalTable: "Certifications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    phone_number = table.Column<string>(type: "char(11)", nullable: false),
+                    email = table.Column<string>(type: "varchar(100)", nullable: false),
+                    PatientClientId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactInfos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContactInfo_PatientClient",
+                        column: x => x.PatientClientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentsTime",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DoctorAttendantId = table.Column<int>(type: "integer", nullable: false),
+                    PatientClientId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentsTime", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppointmentTime_DoctorAttendant",
+                        column: x => x.DoctorAttendantId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppointmentTime_PatientClient",
+                        column: x => x.PatientClientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DoctorAttendantSpeciality",
+                columns: table => new
+                {
+                    DoctorsId = table.Column<int>(type: "integer", nullable: false),
+                    SpecialitiesId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoctorAttendantSpeciality", x => new { x.DoctorsId, x.SpecialitiesId });
+                    table.ForeignKey(
+                        name: "FK_DoctorAttendantSpeciality_Doctors_DoctorsId",
+                        column: x => x.DoctorsId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DoctorAttendantSpeciality_Specialities_SpecialitiesId",
+                        column: x => x.SpecialitiesId,
+                        principalTable: "Specialities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DoctorAttendantId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DoctorAttendant_Schedule",
+                        column: x => x.DoctorAttendantId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentsTime_DoctorAttendantId",
+                table: "AppointmentsTime",
+                column: "DoctorAttendantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentsTime_PatientClientId",
+                table: "AppointmentsTime",
+                column: "PatientClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactInfos_PatientClientId",
+                table: "ContactInfos",
+                column: "PatientClientId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoctorAttendantSpeciality_SpecialitiesId",
+                table: "DoctorAttendantSpeciality",
+                column: "SpecialitiesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_CertificationId",
+                table: "Doctors",
+                column: "CertificationId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_DoctorAttendantId",
+                table: "Schedules",
+                column: "DoctorAttendantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppointmentsTime");
+
+            migrationBuilder.DropTable(
+                name: "ContactInfos");
+
+            migrationBuilder.DropTable(
+                name: "DoctorAttendantSpeciality");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+
+            migrationBuilder.DropTable(
+                name: "Specialities");
+
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Certifications");
+        }
+    }
+}
