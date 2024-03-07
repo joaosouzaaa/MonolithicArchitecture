27eba399696dfcd1cbea67e877dc142f419f85cf
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-06 22:05:55 -0300

    Feat: Adding Repositories DependencyInjection.

---
+++ MonolithicArchitecture.API/DependencyInjection/RepositoriesDependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MonolithicArchitecture.API.Data.Repositories;
+using MonolithicArchitecture.API.Interfaces.Repositories;
+
+namespace MonolithicArchitecture.API.DependencyInjection;
+
+internal static class RepositoriesDependencyInjection
+{
+    internal static void AddRepositoriesDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<IAppointmentTimeRepository, AppointmentTimeRepository>();
+        services.AddScoped<IDoctorAttendantRepository, DoctorAttendantRepository>();
+        services.AddScoped<IPatientClientRepository, PatientClientRepository>();
+        services.AddScoped<IScheduleRepository, ScheduleRepository>();
+        services.AddScoped<ISpecialityRepository, SpecialityRepository>();
+    }
+}
