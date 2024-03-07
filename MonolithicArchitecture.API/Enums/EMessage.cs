b1de84928fd90617f75d9f5d0b986b293da8bc91
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-06 21:17:18 -0300

    Feat: Adding EMessage and Extensions for pattern matching.

---
+++ MonolithicArchitecture.API/Enums/EMessage.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace MonolithicArchitecture.API.Enums;
+public enum EMessage : ushort
+{
+    [Description("{0} has to be greater than {1}.")]
+    GreaterThan,
+
+    [Description("{0} already exists.")]
+    Exists,
+
+    [Description("{0} has invalid length. It should be {1}.")]
+    InvalidLength,
+
+    [Description("{0} needs to be filled.")]
+    Required,
+
+    [Description("{0} was not found.")]
+    NotFound,
+
+    [Description("{0} has invalid format.")]
+    InvalidFormat
+}
