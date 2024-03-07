14774d51d32a81b1ae03252310e423cf623691aa
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-06 21:28:28 -0300

    Feat: Adding DbContext and ConnectionStringFactory.

--- MonolithicArchitecture.API/DependencyInjection/DependencyInjectionHandler.cs
+++ MonolithicArchitecture.API/DependencyInjection/DependencyInjectionHandler.cs
@@ -1,10 +1,21 @@
-﻿namespace MonolithicArchitecture.API.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using MonolithicArchitecture.API.Data.DatabaseContexts;
+using MonolithicArchitecture.API.Factories;
+
+namespace MonolithicArchitecture.API.DependencyInjection;
 
 public static class DependencyInjectionHandler
 {
     public static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCorsDependencyInjection();
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString());
+            options.EnableSensitiveDataLogging();
+            options.EnableDetailedErrors();
+        });
 
         services.AddSettingsDependencyInjection();
         services.AddFilterDependencyInjection();
