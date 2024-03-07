d4d87e6f839532aabbedcb34cccfb0bd15cf2d5b
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-06 21:52:02 -0300

    Feat: Adding Repositories Interfaces.

---
+++ MonolithicArchitecture.API/Interfaces/Repositories/IAppointmentTimeRepository.cs
@@ -0,0 +1,8 @@
+﻿using MonolithicArchitecture.API.Entities;
+
+namespace MonolithicArchitecture.API.Interfaces.Repositories;
+public interface IAppointmentTimeRepository
+{
+    Task<bool> AddAsync(AppointmentTime appointmentTime);
+    Task<bool> ExistsByTimeAndDoctorAsync(int doctorAttendantId, DateTime time);
+}
---
+++ MonolithicArchitecture.API/Interfaces/Repositories/IScheduleRepository.cs
@@ -0,0 +1,7 @@
+﻿using MonolithicArchitecture.API.Entities;
+
+namespace MonolithicArchitecture.API.Interfaces.Repositories;
+public interface IScheduleRepository
+{
+    Task<bool> AddAsync(Schedule schedule);
+}
