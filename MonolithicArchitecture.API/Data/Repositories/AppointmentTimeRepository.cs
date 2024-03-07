90b85f25b86f0a5f11c584a1c3de2db54383322a
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-06 22:02:29 -0300

    Feat: Adding Repositories concrete classes and BaseRepository abstract class.

---
+++ MonolithicArchitecture.API/Data/Repositories/AppointmentTimeRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MonolithicArchitecture.API.Data.DatabaseContexts;
+using MonolithicArchitecture.API.Data.Repositories.BaseRepositories;
+using MonolithicArchitecture.API.Entities;
+using MonolithicArchitecture.API.Interfaces.Repositories;
+
+namespace MonolithicArchitecture.API.Data.Repositories;
+public sealed class AppointmentTimeRepository(AppDbContext dbContext) : BaseRepository<AppointmentTime>(dbContext), IAppointmentTimeRepository
+{
+    public async Task<bool> AddAsync(AppointmentTime appointmentTime)
+    {
+        await DbContextSet.AddAsync(appointmentTime);
+
+        return await SaveChangesAsync();
+    }
+
+    public Task<bool> ExistsByTimeAndDoctorAsync(int doctorAttendantId, DateTime time) =>
+        DbContextSet.AnyAsync(a => a.DoctorAttendantId == doctorAttendantId && a.Time == time);
+}
---
+++ MonolithicArchitecture.API/Data/Repositories/PatientClientRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MonolithicArchitecture.API.Data.DatabaseContexts;
+using MonolithicArchitecture.API.Data.Repositories.BaseRepositories;
+using MonolithicArchitecture.API.Entities;
+using MonolithicArchitecture.API.Interfaces.Repositories;
+
+namespace MonolithicArchitecture.API.Data.Repositories;
+public sealed class PatientClientRepository : BaseRepository<PatientClient>, IPatientClientRepository
+{
+    public PatientClientRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<bool> AddAsync(PatientClient patientClient)
+    {
+        await DbContextSet.AddAsync(patientClient);
+
+        return await SaveChangesAsync();
+    }
+
+    public async Task<bool> UpdateAsync(PatientClient patientClient)
+    {
+        _dbContext.Entry(patientClient.ContactInfo).State = EntityState.Modified;
+        _dbContext.Entry(patientClient).State = EntityState.Modified;
+
+        return await SaveChangesAsync();
+    }
+
+    public Task<PatientClient?> GetByIdAsync(int id, bool asNoTracking)
+    {
+        var query = (IQueryable<PatientClient>)DbContextSet;
+
+        if (asNoTracking)
+            query = DbContextSet.AsNoTracking();
+
+        return query.Include(p => p.ContactInfo).FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public Task<bool> ExistsAsync(int id) =>
+        DbContextSet.AsNoTracking().AnyAsync(p => p.Id == id);
+}
