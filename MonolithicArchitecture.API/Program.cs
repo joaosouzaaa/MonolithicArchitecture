a0672a9e9a98127d8cce57ec62da58f340994224
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-06 21:12:06 -0300

    Feat: Adding UnexpectedErrorMiddleware.

--- MonolithicArchitecture.API/Program.cs
+++ MonolithicArchitecture.API/Program.cs
@@ -1,5 +1,6 @@
 using MonolithicArchitecture.API.Constants;
 using MonolithicArchitecture.API.DependencyInjection;
+using MonolithicArchitecture.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 IConfiguration configuration = builder.Configuration;
@@ -15,6 +16,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.UseMiddleware<UnexpectedErrorMiddleware>();
 }
 
 app.UseHttpsRedirection();
