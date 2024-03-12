b4ace76c21af1476fad83bf1bcd3500350a8fc39
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-11 21:15:23 -0300

    Tests: Mappers Tests.

---
+++ UnitTests/MappersTests/ScheduleMapperTests.cs
@@ -0,0 +1,30 @@
+﻿using MonolithicArchitecture.API.Entities;
+using MonolithicArchitecture.API.Mappers;
+using UnitTests.TestBuilders;
+
+namespace UnitTests.MappersTests;
+public sealed class ScheduleMapperTests
+{
+    private readonly ScheduleMapper _scheduleMapper;
+
+    public ScheduleMapperTests()
+    {
+        _scheduleMapper = new ScheduleMapper();
+    }
+
+    [Fact]
+    public void DomainListToResponseList_SuccessfulScenario()
+    {
+        // A
+        var scheduleList = new List<Schedule>()
+        {
+            ScheduleBuilder.NewObject().DomainBuild()
+        };
+
+        // A
+        var scheduleResponseListResult = _scheduleMapper.DomainListToResponseList(scheduleList);
+
+        // A
+        Assert.Equal(scheduleResponseListResult.Count, scheduleList.Count);
+    }
+}
