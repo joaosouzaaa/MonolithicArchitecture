0a74523d49518c226259e20cd0bf7ad9a365afd8
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-12 22:09:59 -0300

    Feat: Adding AppointmentTimeController, DoctorAttendantController, PatientClientController and SpecialityController concrete classes.

---
+++ MonolithicArchitecture.API/Controllers/PatientClientController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonolithicArchitecture.API.DataTransferObjects.PatientClient;
+using MonolithicArchitecture.API.Interfaces.Services;
+using MonolithicArchitecture.API.Settings.NotificationSettings;
+
+namespace MonolithicArchitecture.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public sealed class PatientClientController(IPatientClientService patientClientService) : ControllerBase
+{
+    [HttpPost("add")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(List<Notification>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public Task<bool> AddAsync([FromBody] PatientClientSave patientClientSave) =>
+        patientClientService.AddAsync(patientClientSave);
+
+    [HttpPut("update")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(List<Notification>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public Task<bool> UpdateAsync([FromBody] PatientClientUpdate patientClientUpdate) =>
+        patientClientService.UpdateAsync(patientClientUpdate);
+
+    [HttpGet("get-by-id")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public Task<PatientClientResponse?> GetByIdAsync([FromQuery] int id) =>
+        patientClientService.GetByIdAsync(id);
+}
---
+++ MonolithicArchitecture.API/Controllers/SpecialityController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonolithicArchitecture.API.DataTransferObjects.Speciality;
+using MonolithicArchitecture.API.Interfaces.Services;
+using MonolithicArchitecture.API.Settings.NotificationSettings;
+
+namespace MonolithicArchitecture.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public sealed class SpecialityController(ISpecialityService specialityService) : ControllerBase
+{
+    [HttpPost("add")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(List<Notification>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public Task<bool> AddAsync([FromBody] SpecialitySave specialitySave) =>
+        specialityService.AddAsync(specialitySave);
+
+    [HttpDelete("delete")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(List<Notification>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public Task<bool> DeleteAsync([FromQuery] int id) =>
+        specialityService.DeleteAsync(id);
+
+    [HttpGet("get-all")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<SpecialityResponse>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public Task<List<SpecialityResponse>> GetAllAsync() =>
+        specialityService.GetAllAsync();
+}
