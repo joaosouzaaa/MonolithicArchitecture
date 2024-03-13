406466ff78aff6d69aaafe8528dc751594cac617
joaosouzaaa <joaoasouza982@gmail.com>
2024-03-12 21:56:34 -0300

    Feat: Adding Services DependencyInjection.

--- MonolithicArchitecture.API/DependencyInjection/ServicesDependencyInjection.cs
+++ MonolithicArchitecture.API/DependencyInjection/ServicesDependencyInjection.cs
@@ -7,7 +7,19 @@ internal static class ServicesDependencyInjection
 {
     internal static void AddServicesDependencyInjection(this IServiceCollection services)
     {
+        services.AddScoped<IAppointmentTimeService, AppointmentTimeService>();
+
+        services.AddScoped<IDoctorAttendantService, DoctorAttendantService>();
+        services.AddScoped<IDoctorAttendantServiceFacade, DoctorAttendantService>();
+
         services.AddScoped<IEmailService, EmailService>();
+
         services.AddScoped<IPatientClientService, PatientClientService>();
+        services.AddScoped<IPatientClientServiceFacade, PatientClientService>();
+
+        services.AddScoped<IScheduleService, ScheduleService>();
+        
+        services.AddScoped<ISpecialityService, SpecialityService>();
+        services.AddScoped<ISpecialityServiceFacade, SpecialityService>();
     }
 }
